37829902b7a1dbcfcf576c5bbbb78351c28dd003
Julien Odent <jodent@fb.com>
2017-05-11 09:30:45 -0700

    CS: Setup + basic Numeral
    Summary:
    * Setup for Czech
    * Basic `Numeral` (0-10 integers + digits) from http://www.omniglot.com/language/numbers/czech.htm
    
    Reviewed By: JonCoens
    
    Differential Revision: D5044775
    
    fbshipit-source-id: b5cd9d2

---
+++ Duckling/Numeral/CS/Rules.hs
@@ -0,0 +1,78 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights can be found in the PATENTS file in the same directory.
+
+
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Duckling.Numeral.CS.Rules
+  ( rules ) where
+
+import Data.HashMap.Strict (HashMap)
+import Data.Maybe
+import Data.String
+import Data.Text (Text)
+import Prelude
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.Text as Text
+
+import Duckling.Dimensions.Types
+import Duckling.Numeral.Helpers
+import Duckling.Numeral.Types (NumeralData (..))
+import Duckling.Regex.Types
+import Duckling.Types
+import qualified Duckling.Numeral.Types as TNumeral
+
+ruleIntegerNumeric :: Rule
+ruleIntegerNumeric = Rule
+  { name = "integer (numeric)"
+  , pattern =
+    [ regex "(\\d{1,18})"
+    ]
+  , prod = \tokens -> case tokens of
+      (Token RegexMatch (GroupMatch (match:_)):
+       _) -> do
+         v <- parseInt match
+         integer $ toInteger v
+      _ -> Nothing
+  }
+
+ruleNumeralMap :: HashMap Text Integer
+ruleNumeralMap = HashMap.fromList
+  [ ( "nula", 0 )
+  , ( "jeden", 1 )
+  , ( "jedna", 1 )
+  , ( "jedno", 1 )
+  , ( "dva", 2 )
+  , ( "dv\x0115", 2 )
+  , ( "t\x0159i", 3 )
+  , ( "\x010dty\x0159i", 4 )
+  , ( "p\x0115t", 5)
+  , ( "\x0161est", 6)
+  , ( "sedm", 7)
+  , ( "osm", 8)
+  , ( "dev\x0115t", 9)
+  , ( "deset", 10)
+  ]
+
+ruleNumeral :: Rule
+ruleNumeral = Rule
+  { name = "number (0..10)"
+  , pattern =
+    [ regex "(nula|jed(en|n[ao])|dv(a|\x0115)|t(\x0159)i|(\x010d)ty(\x0159)i|p(\x0115)t|(\x0161)est|sedm|osm|dev(\x0115)t|deset)"
+    ]
+  , prod = \tokens -> case tokens of
+      (Token RegexMatch (GroupMatch (match:_)):_) ->
+        HashMap.lookup (Text.toLower match) ruleNumeralMap >>= integer
+      _ -> Nothing
+  }
+
+rules :: [Rule]
+rules =
+  [ ruleIntegerNumeric
+  , ruleNumeral
+  ]

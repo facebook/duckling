e30cff7cec84815b40a6430bc6a5e4a8415028da
kkpoon <noopkk@gmail.com>
2017-05-23 16:32:26 +0800

    Add Cantonese support

--- Duckling/Duration/ZH/Corpus.hs
+++ Duckling/Duration/ZH/Corpus.hs
@@ -27,13 +27,25 @@ allExamples = concat
   [ examples (DurationData 1 Second)
              [ "1 秒钟"
              , "一 秒鐘"
+             , "一 秒"
+             ]
+  , example (DurationData 1 Minute)
+             [ "1 分鐘"
+             , "一 分鐘"
+             ]
+  , example (DurationData 1 Hour)
+             [ "1 小時"
+             , "一 小時"
+             , "一 個鐘"
              ]
   , examples (DurationData 5 Day)
              [ "5 天"
              , "五 天"
+             , "五 日"
              ]
   , examples (DurationData 10 Month)
              [ "10 月"
              , "十 月"
+             , "十 個月"
              ]
   ]

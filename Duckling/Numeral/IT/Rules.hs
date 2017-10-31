67190c4238f70b2a2d74c2c00b614613efb5673b
Jeffrey Karres <jkarres@fb.com>
2017-10-30 17:44:37 -0700

    HashMap lookups for large regexes
    Summary: Replacing guards with hashmap
    
    Reviewed By: patapizza
    
    Differential Revision: D6192748
    
    fbshipit-source-id: 644825b

--- Duckling/Numeral/IT/Rules.hs
+++ Duckling/Numeral/IT/Rules.hs
@@ -13,8 +13,11 @@ module Duckling.Numeral.IT.Rules
   ( rules
   ) where
 
+import Data.HashMap.Strict (HashMap)
 import Data.String
+import Data.Text (Text)
 import Prelude
+import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Text as Text
 
 import Duckling.Dimensions.Types
@@ -74,6 +77,18 @@ ruleDecimalNumeral = Rule
       _ -> Nothing
   }
 
+tensMap :: HashMap Text Integer
+tensMap = HashMap.fromList
+  [ ("venti", 20)
+  , ("trenta", 30)
+  , ("quaranta", 40)
+  , ("cinquanta", 50)
+  , ("sessanta", 60)
+  , ("settanta", 70)
+  , ("ottanta", 80)
+  , ("novanta", 90)
+  ]
+
 ruleNumeral2 :: Rule
 ruleNumeral2 = Rule
   { name = "number (20..90)"
@@ -81,18 +96,37 @@ ruleNumeral2 = Rule
     [ regex "(venti|trenta|quaranta|cinquanta|sessanta|settanta|ottanta|novanta)"
     ]
   , prod = \tokens -> case tokens of
-      (Token RegexMatch (GroupMatch (match:_)):_) -> case Text.toLower match of
-        "venti" -> integer 20
-        "trenta" -> integer 30
-        "quaranta" -> integer 40
-        "cinquanta" -> integer 50
-        "sessanta" -> integer 60
-        "settanta" -> integer 70
-        "ottanta" -> integer 80
-        "novanta" -> integer 90
-        _ -> Nothing
+      (Token RegexMatch (GroupMatch (match:_)):_) ->
+        HashMap.lookup (Text.toLower match) tensMap >>= integer
       _ -> Nothing
   }
+
+zeroNineteenMap :: HashMap Text Integer
+zeroNineteenMap = HashMap.fromList
+  [ ("zero", 0)
+  , ("niente", 0)
+  , ("nulla", 0)
+  , ("un", 1)
+  , ("uno", 1)
+  , ("due", 2)
+  , ("tre", 3)
+  , ("quattro", 4)
+  , ("cinque", 5)
+  , ("sei", 6)
+  , ("sette", 7)
+  , ("otto", 8)
+  , ("nove", 9)
+  , ("dieci", 10)
+  , ("undici", 11)
+  , ("dodici", 12)
+  , ("tredici", 13)
+  , ("quattordici", 14)
+  , ("quindici", 15)
+  , ("sedici", 16)
+  , ("diciassette", 17)
+  , ("diciotto", 18)
+  , ("diciannove", 19)
+  ]
 
 ruleNumeral :: Rule
 ruleNumeral = Rule
@@ -101,33 +135,25 @@ ruleNumeral = Rule
     [ regex "(zero|nulla|niente|uno|due|tredici|tre|quattro|cinque|sei|sette|otto|nove|dieci|undici|dodici|quattordici|quindici|sedici|diciassette|diciotto|diciannove|un)"
     ]
   , prod = \tokens -> case tokens of
-      (Token RegexMatch (GroupMatch (match:_)):_) -> case Text.toLower match of
-        "zero" -> integer 0
-        "niente" -> integer 0
-        "nulla" -> integer 0
-        "un" -> integer 1
-        "uno" -> integer 1
-        "due" -> integer 2
-        "tre" -> integer 3
-        "quattro" -> integer 4
-        "cinque" -> integer 5
-        "sei" -> integer 6
-        "sette" -> integer 7
-        "otto" -> integer 8
-        "nove" -> integer 9
-        "dieci" -> integer 10
-        "undici" -> integer 11
-        "dodici" -> integer 12
-        "tredici" -> integer 13
-        "quattordici" -> integer 14
-        "quindici" -> integer 15
-        "sedici" -> integer 16
-        "diciassette" -> integer 17
-        "diciotto" -> integer 18
-        "diciannove" -> integer 19
-        _ -> Nothing
+      (Token RegexMatch (GroupMatch (match:_)):_) ->
+        HashMap.lookup (Text.toLower match) zeroNineteenMap >>= integer
       _ -> Nothing
   }
+
+hundredsMap :: HashMap Text Integer
+hundredsMap = HashMap.fromList
+  [ ("cento", 100)
+  , ("duecento", 200)
+  , ("trecento", 300)
+  , ("quattrocento", 400)
+  , ("cinquecento", 500)
+  , ("seicento", 600)
+  , ("settecento", 700)
+  , ("ottocento", 800)
+  , ("novecento", 900)
+  , ("mila", 1000)
+  , ("mille", 1000)
+  ]
 
 ruleNumeral5 :: Rule
 ruleNumeral5 = Rule
@@ -136,19 +162,8 @@ ruleNumeral5 = Rule
     [ regex "(due|tre|quattro|cinque|sei|sette|otto|nove)?cento|mil(a|le)"
     ]
   , prod = \tokens -> case tokens of
-      (Token RegexMatch (GroupMatch (match:_)):_) -> case Text.toLower match of
-        "cento" -> integer 100
-        "duecento" -> integer 200
-        "trecento" -> integer 300
-        "quattrocento" -> integer 400
-        "cinquecento" -> integer 500
-        "seicento" -> integer 600
-        "settecento" -> integer 700
-        "ottocento" -> integer 800
-        "novecento" -> integer 900
-        " mila" -> integer 1000
-        "mille" -> integer 1000
-        _ -> Nothing
+      (Token RegexMatch (GroupMatch (match:_)):_) ->
+        HashMap.lookup (Text.toLower match) hundredsMap >>= integer
       _ -> Nothing
   }
 
@@ -186,6 +201,90 @@ ruleNumeralsSuffixesKMG = Rule
       _ -> Nothing
   }
 
+twentyoneNinetynineMap :: HashMap Text Integer
+twentyoneNinetynineMap = HashMap.fromList
+  [ ("ventuno", 21)
+  , ("ventidue", 22)
+  , ("ventitre", 23)
+  , ("ventitré", 23)
+  , ("ventiquattro", 24)
+  , ("venticinque", 25)
+  , ("ventisei", 26)
+  , ("ventisette", 27)
+  , ("ventotto", 28)
+  , ("ventinove", 29)
+  , ("trentuno", 31)
+  , ("trentadue", 32)
+  , ("trentatre", 33)
+  , ("trentatré", 33)
+  , ("trentaquattro", 34)
+  , ("trentacinque", 35)
+  , ("trentasei", 36)
+  , ("trentasette", 37)
+  , ("trentotto", 38)
+  , ("trentanove", 39)
+  , ("quarantuno", 41)
+  , ("quarantadue", 42)
+  , ("quarantatre", 43)
+  , ("quarantatré", 43)
+  , ("quarantaquattro", 44)
+  , ("quarantacinque", 45)
+  , ("quarantasei", 46)
+  , ("quarantasette", 47)
+  , ("quarantotto", 48)
+  , ("quarantanove", 49)
+  , ("cinquantuno", 51)
+  , ("cinquantadue", 52)
+  , ("cinquantatre", 53)
+  , ("cinquantatré", 53)
+  , ("cinquantaquattro", 54)
+  , ("cinquantacinque", 55)
+  , ("cinquantasei", 56)
+  , ("cinquantasette", 57)
+  , ("cinquantotto", 58)
+  , ("cinquantanove", 59)
+  , ("sessantuno", 61)
+  , ("sessantadue", 62)
+  , ("sessantatré", 63)
+  , ("sessantatre", 63)
+  , ("sessantaquattro", 64)
+  , ("sessantacinque", 65)
+  , ("sessantasei", 66)
+  , ("sessantasette", 67)
+  , ("sessantotto", 68)
+  , ("sessantanove", 69)
+  , ("settantuno", 71)
+  , ("settantadue", 72)
+  , ("settantatré", 73)
+  , ("settantatre", 73)
+  , ("settantaquattro", 74)
+  , ("settantacinque", 75)
+  , ("settantasei", 76)
+  , ("settantasette", 77)
+  , ("settantotto", 78)
+  , ("settantanove", 79)
+  , ("ottantuno", 81)
+  , ("ottantadue", 82)
+  , ("ottantatré", 83)
+  , ("ottantatre", 83)
+  , ("ottantaquattro", 84)
+  , ("ottantacinque", 85)
+  , ("ottantasei", 86)
+  , ("ottantasette", 87)
+  , ("ottantotto", 88)
+  , ("ottantanove", 89)
+  , ("novantuno", 91)
+  , ("novantadue", 92)
+  , ("novantatre", 93)
+  , ("novantatré", 93)
+  , ("novantaquattro", 94)
+  , ("novantacinque", 95)
+  , ("novantasei", 96)
+  , ("novantasette", 97)
+  , ("novantotto", 98)
+  , ("novantanove", 99)
+  ]
+
 ruleNumeral4 :: Rule
 ruleNumeral4 = Rule
   { name = "number (21..29 31..39 41..49 51..59 61..69 71..79 81..89 91..99)"
@@ -193,88 +292,8 @@ ruleNumeral4 = Rule
     [ regex "((venti|trenta|quaranta|cinquanta|sessanta|settanta|ottanta|novanta)(due|tre|tré|quattro|cinque|sei|sette|nove))|((vent|trent|quarant|cinquant|sessant|settant|ottant|novant)(uno|otto))"
     ]
   , prod = \tokens -> case tokens of
-      (Token RegexMatch (GroupMatch (match:_)):_) -> case Text.toLower match of
-        "ventuno" -> integer 21
-        "ventidue" -> integer 22
-        "ventitre" -> integer 23
-        "ventitré" -> integer 23
-        "ventiquattro" -> integer 24
-        "venticinque" -> integer 25
-        "ventisei" -> integer 26
-        "ventisette" -> integer 27
-        "ventotto" -> integer 28
-        "ventinove" -> integer 29
-        "trentuno" -> integer 31
-        "trentadue" -> integer 32
-        "trentatre" -> integer 33
-        "trentatré" -> integer 33
-        "trentaquattro" -> integer 34
-        "trentacinque" -> integer 35
-        "trentasei" -> integer 36
-        "trentasette" -> integer 37
-        "trentotto" -> integer 38
-        "trentanove" -> integer 39
-        "quarantuno" -> integer 41
-        "quarantadue" -> integer 42
-        "quarantatre" -> integer 43
-        "quarantatré" -> integer 43
-        "quarantaquattro" -> integer 44
-        "quarantacinque" -> integer 45
-        "quarantasei" -> integer 46
-        "quarantasette" -> integer 47
-        "quarantotto" -> integer 48
-        "quarantanove" -> integer 49
-        "cinquantuno" -> integer 51
-        "cinquantadue" -> integer 52
-        "cinquantatre" -> integer 53
-        "cinquantatré" -> integer 53
-        "cinquantaquattro" -> integer 54
-        "cinquantacinque" -> integer 55
-        "cinquantasei" -> integer 56
-        "cinquantasette" -> integer 57
-        "cinquantotto" -> integer 58
-        "cinquantanove" -> integer 59
-        "sessantuno" -> integer 61
-        "sessantadue" -> integer 62
-        "sessantatré" -> integer 63
-        "sessantatre" -> integer 63
-        "sessantaquattro" -> integer 64
-        "sessantacinque" -> integer 65
-        "sessantasei" -> integer 66
-        "sessantasette" -> integer 67
-        "sessantotto" -> integer 68
-        "sessantanove" -> integer 69
-        "settantuno" -> integer 71
-        "settantadue" -> integer 72
-        "settantatré" -> integer 73
-        "settantatre" -> integer 73
-        "settantaquattro" -> integer 74
-        "settantacinque" -> integer 75
-        "settantasei" -> integer 76
-        "settantasette" -> integer 77
-        "settantotto" -> integer 78
-        "settantanove" -> integer 79
-        "ottantuno" -> integer 81
-        "ottantadue" -> integer 82
-        "ottantatré" -> integer 83
-        "ottantatre" -> integer 83
-        "ottantaquattro" -> integer 84
-        "ottantacinque" -> integer 85
-        "ottantasei" -> integer 86
-        "ottantasette" -> integer 87
-        "ottantotto" -> integer 88
-        "ottantanove" -> integer 89
-        "novantuno" -> integer 91
-        "novantadue" -> integer 92
-        "novantatre" -> integer 93
-        "novantatré" -> integer 93
-        "novantaquattro" -> integer 94
-        "novantacinque" -> integer 95
-        "novantasei" -> integer 96
-        "novantasette" -> integer 97
-        "novantotto" -> integer 98
-        "novantanove" -> integer 99
-        _ -> Nothing
+      (Token RegexMatch (GroupMatch (match:_)):_) ->
+        HashMap.lookup (Text.toLower match) twentyoneNinetynineMap >>= integer
       _ -> Nothing
   }
 

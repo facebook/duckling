5186ca9424d1aea1fe1a897776d93867a3c860bb
Igor Drozdov <ihardrozdov@gmail.com>
2017-11-05 17:06:05 +0300

    Added Volume Dimension for Russian (RU) language

--- Duckling/Rules/RU.hs
+++ Duckling/Rules/RU.hs
@@ -21,6 +21,7 @@ import Duckling.Types
 import qualified Duckling.Distance.RU.Rules as Distance
 import qualified Duckling.Numeral.RU.Rules as Numeral
 import qualified Duckling.Ordinal.RU.Rules as Ordinal
+import qualified Duckling.Volume.RU.Rules as Volume
 
 defaultRules :: Some Dimension -> [Rule]
 defaultRules = langRules
@@ -42,4 +43,4 @@ langRules (This Temperature) = []
 langRules (This Time) = []
 langRules (This TimeGrain) = []
 langRules (This Url) = []
-langRules (This Volume) = []
+langRules (This Volume) = Volume.rules

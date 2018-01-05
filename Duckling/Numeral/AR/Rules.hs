ce2f48fbb5b6474a58d806d3b454dab1a2582813
Abdallatif Sulaiman <abd1allatif@gmail.com>
2018-01-05 11:45:22 -0800

    Quickfix for Arabic Numeral
    Summary: Closes https://github.com/facebook/duckling/pull/137
    
    Reviewed By: adelnobel
    
    Differential Revision: D6660959
    
    Pulled By: patapizza
    
    fbshipit-source-id: 51169c63558b5a35f16bdaaada9649c88a4d2994

--- Duckling/Numeral/AR/Rules.hs
+++ Duckling/Numeral/AR/Rules.hs
@@ -13,9 +13,12 @@ module Duckling.Numeral.AR.Rules
   ( rules
   ) where
 
+import Data.HashMap.Strict (HashMap)
 import Data.Maybe
 import Data.String
+import Data.Text (Text)
 import Prelude
+import qualified Data.HashMap.Strict as HashMap
 import qualified Data.Text as Text
 
 import Duckling.Dimensions.Types
@@ -54,10 +57,23 @@ ruleInteger18 :: Rule
 ruleInteger18 = Rule
   { name = "integer 12"
   , pattern =
-    [ regex "(إثن(ت)?[يى] عشر)"
+    [ regex "([إا]?ثن(ت)?[يىا] ?عشر[ةه]?)"
     ]
   , prod = \_ -> integer 12
   }
+
+digitsMap :: HashMap Text Integer
+digitsMap = HashMap.fromList
+  [ ("عشر", 2)
+  , ("ثلاث", 3)
+  , ("اربع", 4)
+  , ("أربع", 4)
+  , ("خمس", 5)
+  , ("ست", 6)
+  , ("سبع", 7)
+  , ("ثمان", 8)
+  , ("تسع", 9)
+  ]
 
 ruleInteger19 :: Rule
 ruleInteger19 = Rule
@@ -66,18 +82,18 @@ ruleInteger19 = Rule
     [ regex "(عشر|ثلاث|[أا]ربع|خمس|ست|سبع|ثمان|تسع)(ون|ين)"
     ]
   , prod = \tokens -> case tokens of
-      Token RegexMatch (GroupMatch (match:_)):_ -> case match of
-        "عشر" -> integer 20
-        "ثلاث" -> integer 30
-        "اربع" -> integer 40
-        "أربع" -> integer 40
-        "خمس" -> integer 50
-        "ست" -> integer 60
-        "سبع" -> integer 70
-        "ثمان" -> integer 80
-        "تسع" -> integer 90
-        _ -> Nothing
+      Token RegexMatch (GroupMatch (match:_)):_ ->
+        (* 10) <$> HashMap.lookup match digitsMap >>= integer
       _ -> Nothing
+  }
+
+ruleInteger200 :: Rule
+ruleInteger200 = Rule
+  { name = "integer (200)"
+  , pattern =
+    [ regex "مائتان|مائتين"
+    ]
+  , prod = const $ integer 200
   }
 
 ruleInteger22 :: Rule
@@ -136,7 +152,7 @@ ruleInteger15 :: Rule
 ruleInteger15 = Rule
   { name = "integer 11"
   , pattern =
-    [ regex "([إاأ]حدى? عشر[ةه]?)"
+    [ regex "([إاأ]حد[يى]? عشر[ةه]?)"
     ]
   , prod = \_ -> integer 11
   }
@@ -157,16 +173,24 @@ rulePowersOfTen :: Rule
 rulePowersOfTen = Rule
   { name = "powers of tens"
   , pattern =
-    [ regex "(مائة|مئات|ألف|الف|آلاف|ملايي(ن)?)"
+    [ regex "(ما?[ئي][ةه]|مئات|[أا]لف|ال??|[آا]لاف|ملايين)"
     ]
   , prod = \tokens -> case tokens of
       (Token RegexMatch (GroupMatch (match:_)):_) -> case Text.toLower match of
+        "مئة" ->
+          double 1e2 >>= withGrain 2 >>= withMultipliable
+        "مئه" ->
+          double 1e2 >>= withGrain 2 >>= withMultipliable
         "مائة" ->
+          double 1e2 >>= withGrain 2 >>= withMultipliable
+        "مائه" ->
           double 1e2 >>= withGrain 2 >>= withMultipliable
         "مئات" ->
           double 1e2 >>= withGrain 2 >>= withMultipliable
         "ألف" -> double 1e3 >>= withGrain 3 >>= withMultipliable
         "الف" -> double 1e3 >>= withGrain 3 >>= withMultipliable
+        "الاف" ->
+          double 1e3 >>= withGrain 3 >>= withMultipliable
         "آلاف" ->
           double 1e3 >>= withGrain 3 >>= withMultipliable
         "ملايي" ->
@@ -181,7 +205,7 @@ ruleInteger3 :: Rule
 ruleInteger3 = Rule
   { name = "integer 2"
   , pattern =
-    [ regex "(اثنان|اثنين)"
+    [ regex "[إا]ثنت?[اي]ن"
     ]
   , prod = \_ -> integer 2
   }
@@ -190,7 +214,7 @@ ruleInteger13 :: Rule
 ruleInteger13 = Rule
   { name = "integer 9"
   , pattern =
-    [ regex "(تسعة|تسع)"
+    [ regex "تسع[ةه]?"
     ]
   , prod = \_ -> integer 9
   }
@@ -199,7 +223,7 @@ ruleInteger12 :: Rule
 ruleInteger12 = Rule
   { name = "integer 8"
   , pattern =
-    [ regex "(ثمانية|ثمان)"
+    [ regex "ثما??ني?[ةه]?"
     ]
   , prod = \_ -> integer 8
   }
@@ -221,7 +245,7 @@ ruleInteger7 :: Rule
 ruleInteger7 = Rule
   { name = "integer 5"
   , pattern =
-    [ regex "خمسة?"
+    [ regex "خمس[ةه]?"
     ]
   , prod = \_ -> integer 5
   }
@@ -230,7 +254,7 @@ ruleInteger14 :: Rule
 ruleInteger14 = Rule
   { name = "integer 10"
   , pattern =
-    [ regex "عشرة?"
+    [ regex "عشر[ةه]?"
     ]
   , prod = \_ -> integer 10
   }
@@ -239,7 +263,7 @@ ruleInteger9 :: Rule
 ruleInteger9 = Rule
   { name = "integer 6"
   , pattern =
-    [ regex "ستة?"
+    [ regex "ست[ةه]?"
     ]
   , prod = \_ -> integer 6
   }
@@ -248,7 +272,7 @@ ruleInteger :: Rule
 ruleInteger = Rule
   { name = "integer 0"
   , pattern =
-    [ regex "(صفر)"
+    [ regex "صفر"
     ]
   , prod = \_ -> integer 0
   }
@@ -266,7 +290,7 @@ ruleInteger2 :: Rule
 ruleInteger2 = Rule
   { name = "integer 1"
   , pattern =
-    [ regex "(واحدة|واحده|واحد)"
+    [ regex "واحد[ةه]?"
     ]
   , prod = \_ -> integer 1
   }
@@ -275,30 +299,9 @@ ruleInteger11 :: Rule
 ruleInteger11 = Rule
   { name = "integer 7"
   , pattern =
-    [ regex "(سبعة|سبع)"
+    [ regex "سبع[ةه]?"
     ]
   , prod = \_ -> integer 7
-  }
-
-ruleInteger20 :: Rule
-ruleInteger20 = Rule
-  { name = "integer (100..900)"
-  , pattern =
-    [ regex "(مائة|مائتان|ثلاثمائة|أربعمائة|خمسمائة|ستمائة|سبعمائة|ثمانمائة|تسعمائة)"
-    ]
-  , prod = \tokens -> case tokens of
-      Token RegexMatch (GroupMatch (match:_)):_ -> case match of
-        "مائة" -> integer 100
-        "سبعمائة" -> integer 700
-        "خمسمائة" -> integer 500
-        "أربعمائة" -> integer 400
-        "ستمائة" -> integer 600
-        "مائتان" -> integer 200
-        "ثلاثمائة" -> integer 300
-        "ثمانمائة" -> integer 800
-        "تسعمائة" -> integer 900
-        _ -> Nothing
-      _ -> Nothing
   }
 
 ruleNumeralDotNumeral :: Rule
@@ -306,7 +309,7 @@ ruleNumeralDotNumeral = Rule
   { name = "number dot number"
   , pattern =
     [ dimension Numeral
-    , regex "فاصلة"
+    , regex "فاصل[ةه]"
     , numberWith TNumeral.grain isNothing
     ]
   , prod = \tokens -> case tokens of
@@ -342,7 +345,6 @@ rules =
   , ruleInteger18
   , ruleInteger19
   , ruleInteger2
-  , ruleInteger20
   , ruleInteger21
   , ruleInteger22
   , ruleInteger23
@@ -356,4 +358,5 @@ rules =
   , ruleNumeralDotNumeral
   , ruleNumeralsPrefixWithMinus
   , rulePowersOfTen
+  , ruleInteger200
   ]

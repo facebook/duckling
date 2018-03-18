1fd61a0af4773932556d08f398ca43bff6fe083d
hend <hend.com13@gmail.com>
2018-03-18 10:43:17 +0200

    add distance to SV

---
+++ Duckling/Distance/SV/Corpus.hs
@@ -0,0 +1,48 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights can be found in the PATENTS file in the same directory.
+
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Duckling.Distance.SV.Corpus
+  ( corpus ) where
+
+import Prelude
+import Data.String
+
+import Duckling.Distance.Types
+import Duckling.Locale
+import Duckling.Resolve
+import Duckling.Testing.Types
+
+corpus :: Corpus
+corpus = (testContext {locale = makeLocale SV Nothing}, allExamples)
+
+allExamples :: [Example]
+allExamples = concat
+  [ examples (simple Kilometre 3)
+             [ "3 kilomètres"
+             , "3 kilometres"
+             , "3 km"
+             , "3km"
+             , "3k"
+             ]
+  , examples (simple Kilometre 3.0)
+             [ "3,0 km"
+             ]
+  , examples (simple Mile 8)
+             [ "8 miles"
+             ]
+  , examples (simple Metre 9)
+             [ "9 metres"
+             , "9m"
+             ]
+  , examples (simple Centimetre 2)
+             [ "2cm"
+             , "2 centimetres"
+             ]
+  ]

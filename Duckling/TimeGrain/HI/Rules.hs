3e085fcfb137b5ee670126c6531ea9de87ee2fbd
Anshuman Chhabra <anshuman.lalakers@gmail.com>
2018-05-22 12:13:04 -0700

    Added TimeGrain to Hindi (HI)
    Summary:
    I have added __TimeGrain__ support to __Hindi (HI)__. Please let me know if there are any other changes required!
    
    Cheers! :)
    Closes https://github.com/facebook/duckling/pull/198
    
    Reviewed By: patapizza
    
    Differential Revision: D8082131
    
    Pulled By: chinmay87
    
    fbshipit-source-id: 73b8263a9f95775c32905edede14113c51b10431

---
+++ Duckling/TimeGrain/HI/Rules.hs
@@ -0,0 +1,42 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree. An additional grant
+-- of patent rights can be found in the PATENTS file in the same directory.
+
+
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE OverloadedStrings #-}
+
+module Duckling.TimeGrain.HI.Rules
+  ( rules
+  ) where
+
+import Data.String
+import Data.Text (Text)
+import Prelude
+
+import Duckling.Dimensions.Types
+import Duckling.Types
+import qualified Duckling.TimeGrain.Types as TG
+
+grains :: [(Text, String, TG.Grain)]
+grains = [ ("second (grain)", "सेकंड", TG.Second)
+         , ("minute (grain)", "मिनट", TG.Minute)
+         , ("hour (grain)", "घंटा", TG.Hour)
+         , ("day (grain)", "(दिन|दिवस)", TG.Day)
+         , ("week (grain)", "(सप्ताह|हफ़्ता)", TG.Week)
+         , ("month (grain)", "महीना", TG.Month)
+         , ("quarter (grain)", "चौथाई", TG.Quarter)
+         , ("year (grain)", "(साल|वर्ष)", TG.Year)
+         ]
+
+rules :: [Rule]
+rules = map go grains
+  where
+    go (name, regexPattern, grain) = Rule
+      { name = name
+      , pattern = [regex regexPattern]
+      , prod = \_ -> Just $ Token TimeGrain grain
+      }

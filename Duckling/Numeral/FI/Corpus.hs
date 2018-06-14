55ca8391c1e7d450018858114f49d39bdfd1898f
Tero Laxström <tlax@neonpeons.com>
2018-06-14 21:07:35 +0300

    Added more tests
    Signed-off-by: Tero Laxström <tlax@neonpeons.com>

--- Duckling/Numeral/FI/Corpus.hs
+++ Duckling/Numeral/FI/Corpus.hs
@@ -25,13 +25,60 @@ allExamples = concat
              [ "1"
              , "yksi"
              ]
+  , examples (NumeralValue 2)
+             [ "kaksi"
+             ]
+  , examples (NumeralValue 3)
+             [ "kolme"
+             ]
+  , examples (NumeralValue 4)
+             [ "neljä"
+             ]
+  , examples (NumeralValue 5)
+             [ "viisi"
+             ]
+  , examples (NumeralValue 6)
+             [ "kuusi"
+             ]
+  , examples (NumeralValue 7)
+             [ "seitsemän"
+             ]
+  , examples (NumeralValue 8)
+             [ "kahdeksan"
+             ]
+  , examples (NumeralValue 9)
+             [ "yhdeksän"
+             ]
   , examples (NumeralValue 10)
              [ "10"
              , "kymmenen"
              ]
+  , examples (NumeralValue 11)
+             [ "yksitoista"
+             ]
+  , examples (NumeralValue 12)
+             [ "kaksitoista"
+             ]
   , examples (NumeralValue 13)
-             [ "13"
-             , "kolmetoista"
+             [ "kolmetoista"
+             ]
+  , examples (NumeralValue 14)
+             [ "neljätoista"
+             ]
+  , examples (NumeralValue 15)
+             [ "viisitoista"
+             ]
+  , examples (NumeralValue 16)
+             [ "kuusitoista"
+             ]
+  , examples (NumeralValue 17)
+             [ "seitsemäntoista"
+             ]
+  , examples (NumeralValue 18)
+             [ "kahdeksantoista"
+             ]
+  , examples (NumeralValue 19)
+             [ "yhdeksäntoista"
              ]
   , examples (NumeralValue 20)
              [ "20"
@@ -42,12 +89,22 @@ allExamples = concat
              , "kaksikymmentäneljä"
              ]
   , examples (NumeralValue 35)
-             [ "35"
-             , "kolmekymmentäviisi"
+             [ "kolmekymmentäviisi"
              ]
   , examples (NumeralValue 42)
-             [ "42"
-             , "neljäkymmentäkaksi"
+             [ "neljäkymmentäkaksi"
+             ]
+  , examples (NumeralValue 52)
+             [ "viisikymmentäkaksi"
+             ]
+  , examples (NumeralValue 62)
+             [ "kuusikymmentäkaksi"
+             ]
+  , examples (NumeralValue 72)
+             [ "seitsemänkymmentäkaksi"
+             ]
+  , examples (NumeralValue 82)
+             [ "kahdeksankymmentäkaksi"
              ]
   , examples (NumeralValue 99)
              [ "99"

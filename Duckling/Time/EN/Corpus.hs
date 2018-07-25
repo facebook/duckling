9a4bb48c63e914af67d00a4e04f73076e6ad95c3
Chinmay Deshmukh <chinmayd@fb.com>
2018-07-25 13:15:04 -0700

    Time: Fix BST and IST offsets
    Summary:
    Fix the offsets for BST and IST timezones.
    BST is +1 (60) and IST is +5:30 (330)
    
    Reviewed By: girifb
    
    Differential Revision: D8981055
    
    fbshipit-source-id: 43ad669c22638031479a7608a8e651b178361c0d

--- Duckling/Time/EN/Corpus.hs
+++ Duckling/Time/EN/Corpus.hs
@@ -1035,12 +1035,14 @@ allExamples = concat
              , "Thursday 8:00 gmt"
              , "Thu at 8 GMT"
              , "Thu at 8 gmt"
+             , "Thursday 9 am BST"
              ]
   , examples (datetime (2013, 2, 14, 14, 0, 0) Minute)
              [ "Thursday 8:00 PST"
              , "Thursday 8:00 pst"
              , "Thu at 8 am PST"
              , "Thu at 8 am pst"
+             , "Thursday at 9:30pm ist"
              ]
   , examples (datetime (2013, 2, 12, 14, 0, 0) Hour)
              [ "today at 2pm"

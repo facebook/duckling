0b57f49360abc7552541722875b92d2660763e09
Phuong Trinh <lolotp@fb.com>
2019-09-26 02:29:33 -0700

    Remove ANN pragma for duckling
    Summary:
    Use of ANN pragma may slow down compilation time because of TemplateHaskell.
    Because of that, using comment style ignore would be preferable.
    
    For more information on ways to ignore hints with hlint, please see
    https://github.com/ndmitchell/hlint#ignoring-hints
    
    Reviewed By: patapizza
    
    Differential Revision: D17365266
    
    fbshipit-source-id: 71e4952738bba17b4d2ec2a18b31b4b7e3f509db

--- Duckling/Time/Types.hs
+++ Duckling/Time/Types.hs
@@ -3,6 +3,7 @@
 --
 -- This source code is licensed under the BSD-style license found in the
 -- LICENSE file in the root directory of this source tree.
+{- HLINT ignore "Use foldr1OrError" -}
 
 
 {-# LANGUAGE DeriveAnyClass #-}
@@ -240,7 +241,6 @@ data Predicate
   | ReplaceIntersectPredicate Predicate Predicate Predicate
   deriving Show
 
-{-# ANN runPredicate ("HLint: ignore Use foldr1OrError" :: String) #-}
 runPredicate :: Predicate -> SeriesPredicate
 runPredicate EmptyPredicate{} = \_ _ -> ([], [])
 runPredicate (SeriesPredicate (NoShow p)) = p

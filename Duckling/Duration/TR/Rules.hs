fdedbe1eb7128e39dcdb4c10f906ee1d5a2b9883
Mustafa ALP <mustafaalp43@gmail.com>
2021-02-27 14:50:20 +0300

    fix: typo

--- Duckling/Duration/TR/Rules.hs
+++ Duckling/Duration/TR/Rules.hs
@@ -43,7 +43,7 @@ ruleDurationHalfAnHour = Rule
 ruleDurationThreeQuartersOfAnHour :: Rule
 ruleDurationThreeQuartersOfAnHour = Rule
   { name = "three-quarters of an hour"
-  , pattern = [regex "(3/4\\s?sa(at)?|üçe \231eyrek sa(at)?)"]
+  , pattern = [regex "(3/4\\s?sa(at)?|üç \231eyrek sa(at)?)"]
   , prod = \_ -> Just . Token Duration $ duration TG.Minute 45
   }
 

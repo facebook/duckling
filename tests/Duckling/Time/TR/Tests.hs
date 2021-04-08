9babaee20ab2e58431d40f8f25f2efa5b609cc55
chessai <chessai1996@gmail.com>
2021-04-08 10:42:39 -0700

    add missing copyright headers (#591)
    Summary: Pull Request resolved: https://github.com/facebook/duckling/pull/591
    
    Reviewed By: patapizza
    
    Differential Revision: D27623732
    
    Pulled By: chessai
    
    fbshipit-source-id: e38a64d04d004ea80fabeb5efbc635b00e2802df

--- tests/Duckling/Time/TR/Tests.hs
+++ tests/Duckling/Time/TR/Tests.hs
@@ -1,3 +1,9 @@
+-- Copyright (c) 2016-present, Facebook, Inc.
+-- All rights reserved.
+--
+-- This source code is licensed under the BSD-style license found in the
+-- LICENSE file in the root directory of this source tree.
+
 module Duckling.Time.TR.Tests
   ( tests ) where
 

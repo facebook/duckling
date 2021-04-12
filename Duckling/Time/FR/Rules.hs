c1619c1a67a78381b4c3745870461c999f39fbdb
chessai <chessai1996@gmail.com>
2021-04-12 15:30:51 -0700

    FR: add nth <time> of <time> rules

--- Duckling/Time/FR/Rules.hs
+++ Duckling/Time/FR/Rules.hs
@@ -24,6 +24,7 @@ import Duckling.Regex.Types
 import Duckling.Time.Helpers
 import Duckling.Time.Types (TimeData (..))
 import Duckling.Types
+import qualified Duckling.Ordinal.Types as TOrdinal
 import qualified Duckling.Time.Types as TTime
 import qualified Duckling.TimeGrain.Types as TG
 
@@ -314,6 +315,37 @@ ruleNDerniersCycle = Rule
       (token:_:Token TimeGrain grain:_) -> do
         n <- getIntValue token
         tt . cycleN True grain $ - n
+      _ -> Nothing
+  }
+
+ruleNthTimeOfTime :: Rule
+ruleNthTimeOfTime = Rule
+  { name = "nth <time> of <time>"
+  , pattern =
+    [ dimension Ordinal
+    , dimension Time
+    , regex "d(e|u)"
+    , dimension Time
+    ]
+  , prod = \tokens -> case tokens of
+      (Token Ordinal od:Token Time td1:_:Token Time td2:_) -> Token Time .
+        predNth (TOrdinal.value od - 1) False <$> intersect td2 td1
+      _ -> Nothing
+  }
+
+ruleTheNthTimeOfTime :: Rule
+ruleTheNthTimeOfTime = Rule
+  { name = "the nth <time> of <time>"
+  , pattern =
+    [ regex "le"
+    , dimension Ordinal
+    , dimension Time
+    , regex "d(e|u)"
+    , dimension Time
+    ]
+  , prod = \tokens -> case tokens of
+      (_:Token Ordinal od:Token Time td1:_:Token Time td2:_) -> Token Time .
+         predNth (TOrdinal.value od - 1) False <$> intersect td2 td1
       _ -> Nothing
   }
 
@@ -2070,6 +2102,8 @@ rules =
   , ruleDbutDAnnee
   , rulePlusTard
   , rulePlusTardPartofday
+  , ruleNthTimeOfTime
+  , ruleTheNthTimeOfTime
   ]
   ++ ruleMonths
   ++ ruleDaysOfWeek
